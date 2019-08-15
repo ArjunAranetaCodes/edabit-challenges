2e50a9ba8213c19bb1a98974f845e883e6e75870
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-08-16 05:00:00 +0800

    Edabit Challenges

---
+++ Very Easy/ES6-Destructuring-Objects-VIII/JavaScript/solution2.js
@@ -0,0 +1,8 @@
+const user = {
+  name: 'John',
+  email: 'john@example.com',
+  city: 'Phoenix',
+  state: 'AZ',
+  country: 'USA',
+};
+const str = `({ name, email, ...rest } = user ).toString()`;
