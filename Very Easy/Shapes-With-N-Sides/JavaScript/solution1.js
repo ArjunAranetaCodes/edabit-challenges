cea4783304e3e387dc3a608fee6c7ba4863d045a
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-09-07 05:00:00 +0800

    Edabit Challenges

---
+++ Very Easy/Shapes-With-N-Sides/JavaScript/solution1.js
@@ -0,0 +1,15 @@
+function nSidedShape(n) {
+	let shapes = {
+		1:	"circle",
+		2:	"semi-circle",
+		3:	"triangle",
+		4:	"square",
+		5:	"pentagon",
+		6:	"hexagon",
+		7:	"heptagon",
+		8:	"octagon",
+		9:	"nonagon",
+		10:	"decagon",
+	}
+	return shapes[n];
+}
