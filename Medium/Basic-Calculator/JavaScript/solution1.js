952ee9797a3d5c1df047f34c5c7bd2fb602dc51f
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-10-10 05:00:00 +0800

    Edabit Challenges

---
+++ Medium/Basic-Calculator/JavaScript/solution1.js
@@ -0,0 +1,8 @@
+function calculator(num1,operator,num2) {
+	return {
+		'+': num1 + num2,
+		'-': num1 - num2,
+		'*': num1 * num2,
+		'/': num2 ? num1 / num2 : 'Cannot divide by 0!'
+	}[operator]
+}
