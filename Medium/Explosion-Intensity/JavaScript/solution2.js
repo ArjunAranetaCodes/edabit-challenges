234991472e1129f4d41a979f91156ce8fc84717c
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-10-16 05:00:00 +0800

    Edabit Challenges

---
+++ Medium/Explosion-Intensity/JavaScript/solution2.js
@@ -0,0 +1,10 @@
+const boomIntensity = num => {
+  if (num < 2) return 'boom'
+  
+  let str = 'B' + 'o'.repeat(num) + 'm'
+  
+  if (num % 2 == 0) str = str + '!'
+  if (num % 5 == 0) str = str.toUpperCase()
+  
+  return str
+}
