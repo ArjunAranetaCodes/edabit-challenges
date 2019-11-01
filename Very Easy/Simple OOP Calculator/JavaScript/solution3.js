df2d5af53cc3f67992d6a742fdb0a007446f1eb3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-11-02 05:00:00 +0800

    Edabit Challenges

---
+++ Very Easy/Simple OOP Calculator/JavaScript/solution3.js
@@ -0,0 +1,15 @@
+class Calculator {
+	// Write functions to add(), subtract(), multiply() and divide()
+ add(num1,num2){
+		return num1+num2;
+	}
+	subtract(num1,num2){
+		return num1-num2;
+	}
+	multiply(num1,num2){
+		return num1*num2;
+	}
+	divide(num1,num2){
+		return num1/num2;
+	}
+}
