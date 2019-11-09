50d0629aaf07c720578e1942f0b5e4ced412a7d8
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-11-10 05:00:00 +0800

    Edabit Challenges

---
+++ Very Hard/Balanced-Words/JavaScript/solution1.js
@@ -0,0 +1,2 @@
+const wordVal = w => [...w].reduce((a, v) => a + ' abcdefghijklmnopqrstuvwxyz'.indexOf(v), 0);
+const balanced = word => wordVal(word.slice(0, word.length >> 1)) === wordVal(word.slice(Math.ceil(word.length / 2)));
