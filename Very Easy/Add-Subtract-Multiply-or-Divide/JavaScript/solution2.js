cdae12010826deeae941b187b5a4cf85293e6a4d
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-12-14 05:00:00 +0800

    Edabit Challenges

---
+++ Very Easy/Add-Subtract-Multiply-or-Divide/JavaScript/solution2.js
@@ -0,0 +1,7 @@
+function operation(num1, num2) {
+	if (num1 + num2 == 24) {return "added"}
+	if (num1 - num2 == 24) {return "subtracted"}
+	if (num1 * num2 == 24) {return "multiplied"}
+	if (num1 / num2 == 24) {return "divided"}
+	return null
+}
