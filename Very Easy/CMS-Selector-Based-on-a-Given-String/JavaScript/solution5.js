b2831ce816825761dd1f41f823c59a6e81c731ee
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-05 05:00:00 +0800

    Edabit Challenges

--- Very Easy/CMS-Selector-Based-on-a-Given-String/JavaScript/solution5.js
+++ Very Easy/CMS-Selector-Based-on-a-Given-String/JavaScript/solution5.js
@@ -1,1 +1,10 @@
-const kmtomiles = km => +(km * 0.62137119).toFixed(5);
+function cmsSelector(arr, str) {
+	var arrCMS = []
+	for(var x = 0; x < arr.length; x++){
+		if(arr[x].includes(str)){
+			arrCMS.push(arr[x])
+		}
+	}
+	arrCMS.sort()
+	return arrCMS
+}
