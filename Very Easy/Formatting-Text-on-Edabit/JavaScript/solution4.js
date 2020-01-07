27911d90958d2a275c61f6cdf3c74a40fcdaa4b6
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-08 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Formatting-Text-on-Edabit/JavaScript/solution4.js
+++ Very Easy/Formatting-Text-on-Edabit/JavaScript/solution4.js
@@ -1,1 +1,12 @@
-findDigitAmount=n=>(''+n).length
+function mdFormat(word, style) {
+	switch(style){
+		case "b":	return (`**${word}**`);
+			break;
+		case "i":	return (`_${word}_`);
+			break;
+		case "c":	return (`\`${word}\``);
+		break;
+		case "s":	return (`~~${word}~~`);
+		break;
+	}
+}
