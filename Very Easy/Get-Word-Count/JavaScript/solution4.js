ee3534edba23ae70849ad7f5f597a9aa8405cee3
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-10 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Get-Word-Count/JavaScript/solution4.js
+++ Very Easy/Get-Word-Count/JavaScript/solution4.js
@@ -1,8 +1,1 @@
-const halfQuarterEighth = n => {
-  let newArray = [];
-  for (let i = 0; i <= 2; i++) {
-    n = n / 2;
-    newArray.push(n);
-  }
-  return newArray;
-};
+countWords = s => s.split` `.length
