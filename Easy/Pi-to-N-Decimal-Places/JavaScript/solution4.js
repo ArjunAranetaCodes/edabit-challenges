d81743f9f9cb335a00a0d5c2e75bdd60a54e6ace
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-11 05:00:00 +0800

    Edabit Challenges

--- Easy/Pi-to-N-Decimal-Places/JavaScript/solution4.js
+++ Easy/Pi-to-N-Decimal-Places/JavaScript/solution4.js
@@ -1,8 +1,3 @@
-function sumOfCubes(nums) {
-	var sum = 0;
-	var l = nums.length;
-	for (var i = 0; i < l; i++){
-		sum += Math.pow(nums[i],3);
-	}
-	return sum;
+function myPi(n) {
+	return parseFloat(Math.PI.toFixed(n))
 }
