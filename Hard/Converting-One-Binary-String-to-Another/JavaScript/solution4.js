cda0c19fbd3637c728dcf3a6367e99297a22970e
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-19 05:00:00 +0800

    Edabit Challenges

--- Hard/Converting-One-Binary-String-to-Another/JavaScript/solution4.js
+++ Hard/Converting-One-Binary-String-to-Another/JavaScript/solution4.js
@@ -1,14 +1,1 @@
-function uncensor(str, vowels) {
-    let vowel_i = 0;
-    let new_str = ""
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == "*") {
-            new_str = new_str + vowels[vowel_i]
-            vowel_i += 1;
-        }
-        else {
-            new_str = new_str + str[i]
-        }
-    }
-    return new_str
-}
+const minSwaps = (a,b) => [...a].filter((e,i) => e!==b[i]).length/2
