1e24ac1eec9b9f9de94cd5f4b056aad664d6abcc
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-20 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Headline-Hash-Tags/JavaScript/solution5.js
+++ Very Hard/Headline-Hash-Tags/JavaScript/solution5.js
@@ -1,22 +1,17 @@
-const prices = {
-	Strawberries: 1.50, Banana: 0.50, Mango: 2.50,
-	Blueberries: 1.00, Raspberries: 1.00, Apple: 1.75,
-	Pineapple: 3.50
-}
-
-class Smoothie {
-	constructor(ingredients) {    
-    this.ingredients = ingredients;
-		this.cost = this.ingredients.map((ing) => prices[ing]).reduce((a, c) => a + c)
-  }
-	getCost() {
-		return "$" + this.cost.toFixed(2)
-  }
-	getPrice() {
-    return "$" + (this.cost + this.cost * 1.5).toFixed(2);
-  }
-	getName() {
-		let ings = this.ingredients.sort().map(function(x){ return x.replace("ies","y")}).join(" ")
-		return this.ingredients.length == 1 ? ings + " Smoothie" : ings + " Fusion";
-  }
+function getHashTags(str) {
+	return str.toLowerCase()
+		.replace(/[,.?]/g,'')
+		.split(/\s+/)
+	  .map(function(x,i){
+		  return [x,i];
+	  })
+		.sort(function(a,b){
+		  var c = b[0].length - a[0].length; 
+		  if (c != 0) return c;
+		  return a[1]-b[1];
+	  })
+		.slice(0,3)
+	  .map(function(x) {
+		  return '#'+x[0];
+	  });
 }
