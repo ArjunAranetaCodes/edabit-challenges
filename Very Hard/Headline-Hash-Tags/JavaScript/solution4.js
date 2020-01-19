149428d23f3577e3ae7a218641b9a53a2d5086d3
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-20 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Headline-Hash-Tags/JavaScript/solution4.js
+++ Very Hard/Headline-Hash-Tags/JavaScript/solution4.js
@@ -1,31 +1,15 @@
-const prices = {
-	Strawberries: 1.50, Banana: 0.50, Mango: 2.50,
-	Blueberries: 1.00, Raspberries: 1.00, Apple: 1.75,
-	Pineapple: 3.50
-}
+const byLength = (a, b) => b.length - a.length;
 
-class Smoothie {
-	constructor(array){
-		this.ingredients = array;
-	}
-	
-	getCost(){
-		var price = 0;
-		for (var ndx = 0; ndx < this.ingredients.length; ndx++){
-			price = price + prices[this.ingredients[ndx]]
-		}
-		price = "$"+price.toFixed(2);
-		return price;
-	}
-	
-	getPrice(){
-		var ingPrice = parseFloat(this.getCost().replace("$",""));
-		return "$" + (ingPrice + (ingPrice * 1.5)).toFixed(2);
-	}
-	
-	getName(){
-		var smoothieName = this.ingredients.map(x => x.replace("ies", "y")).sort();
-		if(smoothieName.length > 1) return smoothieName.join(" ") + " Fusion";
-		return smoothieName + " Smoothie";
-	}
-}
+const getHashTags = str => {
+  const words = str.match(/\w+/g);
+  const result = [...words.slice(0, 3)].sort(byLength);
+
+  words.slice(3).forEach(word => {
+    if (result.some(({ length }) => length < word.length)) {
+      result[2] = word;
+      result.sort(byLength);
+    }
+  });
+
+  return result.map(word => `#${word.toLowerCase()}`);
+};
