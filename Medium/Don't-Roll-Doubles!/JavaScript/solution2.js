5b2295a2d284b9706644f234332b811668f5fde1
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-26 05:00:00 +0800

    Edabit Challenges

--- Medium/Don't-Roll-Doubles!/JavaScript/solution2.js
+++ Medium/Don't-Roll-Doubles!/JavaScript/solution2.js
@@ -1,2 +1,3 @@
-const filterUnique = arr =>
-arr.filter(x => new Set(x).size === x.length);
+function diceGame(arr) {
+	return arr.every(e => e[0] != e[1]) ? arr.flat().reduce((a,b) => a+b,0) : 0;
+}
