a95d919ef6b03b5b3cae2352ac6604ee4aa25bca
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-26 05:00:00 +0800

    Edabit Challenges

--- Medium/Don't-Roll-Doubles!/JavaScript/solution5.js
+++ Medium/Don't-Roll-Doubles!/JavaScript/solution5.js
@@ -1,5 +1,12 @@
-function filterUnique(arr){
-    return arr.filter((str) => {
-        return str.length === new Set(str).size
-    })
+function diceGame(arr) {
+	var newArr = arr.flat(Infinity)
+	var sum = 0
+	for(var x = 0; x < newArr.length; x++){
+		if(newArr[x] === newArr[x+1]){
+			return 0
+		}else{
+			sum = sum + newArr[x]
+		}
+	}
+	return sum
 }
