928a89b0889bcef72eb430a9f959b23b9652b062
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-27 05:00:00 +0800

    Edabit Challenges

--- Medium/Promises-II-What-Is-a-Callback/JavaScript/solution5.js
+++ Medium/Promises-II-What-Is-a-Callback/JavaScript/solution5.js
@@ -1,12 +1,12 @@
-function diceGame(arr) {
-	var newArr = arr.flat(Infinity)
-	var sum = 0
-	for(var x = 0; x < newArr.length; x++){
-		if(newArr[x] === newArr[x+1]){
-			return 0
-		}else{
-			sum = sum + newArr[x]
-		}
-	}
-	return sum
+function anotherFunc() {
+	let str = "bye"
+	setTimeout(() => {
+		callback(str)
+	}, 100)
+}
+
+var doc = "hello"
+
+function callback(str) {
+	doc = str
 }
