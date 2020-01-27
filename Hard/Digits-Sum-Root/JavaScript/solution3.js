756f4924ab7d155f71df10cf4e0ef042bccaa470
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-28 05:00:00 +0800

    Edabit Challenges

--- Hard/Digits-Sum-Root/JavaScript/solution3.js
+++ Hard/Digits-Sum-Root/JavaScript/solution3.js
@@ -1,9 +1,12 @@
-function minSwaps(s1, s2) {
-	let count = 0;
-	for(let i = 0; i < s1.length; i++){
-		if(s1[i] !== s2[i]){
-			count++;
+function rootDigit(n) {
+	var arrN = n.toString().split("")
+	var sum = 0
+	while(arrN.length > 1){
+		sum = 0
+		for(var x= 0; x < arrN.length; x++){
+			sum = sum + parseInt(arrN[x])
 		}
+		arrN = sum.toString().split("")		
 	}
-	return count/2;
+	return sum
 }
