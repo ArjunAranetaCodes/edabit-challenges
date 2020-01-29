cc5c81c4297ca06695b6fde3a98868c2ba01cc86
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-30 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Numbers-in-Strings/JavaScript/solution3.js
+++ Very Hard/Numbers-in-Strings/JavaScript/solution3.js
@@ -1,21 +1,9 @@
-function getHashTags(str) {
-	var newArr = str.split(" ").sort(function(a, b){
-		return b.length - a.length;
-	});
-	
-	newArr = addHashtag(newArr)
-	
-	if(newArr.length < 3){
-		return newArr;
-	}
-	return [newArr[0], newArr[1], newArr[2]]
-}
+const sum = arr => arr.reduce((total, num) => total + num, 0);
 
-function addHashtag(arr){
-	var newArr = []
-	for(var x = 0; x < arr.length; x++){
-		var word = "#" + arr[x].toLowerCase().replace(/[^a-zA-Z ]/g, "")
-		newArr.push(word)
-	}
-	return newArr
-}
+const getDigits = num => String(num).split('').map(Number);
+
+const happy = num => {
+  if (num === 1) return true;
+  if (num === 4) return false;
+  return happy(sum(getDigits(num).map(digit => Math.pow(digit, 2))));
+};
