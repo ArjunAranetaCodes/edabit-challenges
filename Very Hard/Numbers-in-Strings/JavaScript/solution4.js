70394095de50e4340f109f267193ba13dac569e7
Arjun Araneta <arjunaraneta@gmail.com>
2020-01-30 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Numbers-in-Strings/JavaScript/solution4.js
+++ Very Hard/Numbers-in-Strings/JavaScript/solution4.js
@@ -1,15 +1,6 @@
-const byLength = (a, b) => b.length - a.length;
-
-const getHashTags = str => {
-  const words = str.match(/\w+/g);
-  const result = [...words.slice(0, 3)].sort(byLength);
-
-  words.slice(3).forEach(word => {
-    if (result.some(({ length }) => length < word.length)) {
-      result[2] = word;
-      result.sort(byLength);
-    }
-  });
-
-  return result.map(word => `#${word.toLowerCase()}`);
-};
+function happy(n) {
+	while (n !==4 && n !==1){
+		n=[...''+n].map(e=>Math.pow(e,2)).reduce((acc, cur)=>acc+ cur,0)
+	}
+	return n ===1 ?true :false
+}
