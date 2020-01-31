2faa31ab8ab818a7a2e2791266e22de8adb4bb07
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-01 05:00:00 +0800

    Edabit Challenges

--- Easy/Join-Two-Portions-of-a-Path/JavaScript/solution4.js
+++ Easy/Join-Two-Portions-of-a-Path/JavaScript/solution4.js
@@ -1,1 +1,5 @@
-const isOmnipresent = (arr, val) => arr.every(subArr => subArr.includes(val));
+function joinPath(portion1, portion2) {
+	let p1 = portion1.includes('/')? portion1.slice(0,-1) : portion1
+	let p2 = portion2.includes('/')? portion2.slice(1) : portion2
+	return `${p1}/${p2}`
+}
