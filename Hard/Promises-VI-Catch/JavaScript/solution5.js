d90b3ee1ffeaf848627dcc16caa88d6e69319575
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-04 05:00:00 +0800

    Edabit Challenges

--- Hard/Promises-VI-Catch/JavaScript/solution5.js
+++ Hard/Promises-VI-Catch/JavaScript/solution5.js
@@ -1,5 +1,5 @@
-function numberOfRepeats(str) {
-	let i = str.length-str.lastIndexOf(str[0]);
-  let sub = str.slice(0,i);
-  return str.replace(RegExp(`(${sub})`,'gi'),'x').length
-}
+let errorLog = {}
+
+let promise = new Promise((resolve, reject) => {
+    throw new Error('Something failed');
+}).catch(err => errorLog = err)
