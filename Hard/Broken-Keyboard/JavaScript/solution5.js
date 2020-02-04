d1b0f23d815f616d2994874dc5c31577ff2a41e9
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-05 05:00:00 +0800

    Edabit Challenges

--- Hard/Broken-Keyboard/JavaScript/solution5.js
+++ Hard/Broken-Keyboard/JavaScript/solution5.js
@@ -1,5 +1,11 @@
-let errorLog = {}
-
-let promise = new Promise((resolve, reject) => {
-    throw new Error('Something failed');
-}).catch(err => errorLog = err)
+function findBrokenKeys(str1, str2) {
+	var arrBroken = []
+	for(var x = 0; x < str1.length; x++){
+		if(str1.charAt(x) !== str2.charAt(x)){
+			if(!arrBroken.includes(str1.charAt(x))){
+			 arrBroken.push(str1.charAt(x))	
+			}			
+		}			
+	}
+	return arrBroken
+}
