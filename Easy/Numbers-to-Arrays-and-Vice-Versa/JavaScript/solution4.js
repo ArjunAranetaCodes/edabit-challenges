32b9368cb361a5dac425bbc80263717b914be920
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-09 05:00:00 +0800

    Edabit Challenges

--- Easy/Numbers-to-Arrays-and-Vice-Versa/JavaScript/solution4.js
+++ Easy/Numbers-to-Arrays-and-Vice-Versa/JavaScript/solution4.js
@@ -1,5 +1,12 @@
-function joinPath(portion1, portion2) {
-	let p1 = portion1.includes('/')? portion1.slice(0,-1) : portion1
-	let p2 = portion2.includes('/')? portion2.slice(1) : portion2
-	return `${p1}/${p2}`
+function toArray(num) {
+	var arrNum = num.toString().split("")
+	var newArr = []
+	for(var x = 0; x < arrNum.length; x++){
+		newArr.push(parseInt(arrNum[x]))
+	}
+	return newArr
+}
+
+function toNumber(arr) {
+	return parseInt(arr.join(""))
 }
