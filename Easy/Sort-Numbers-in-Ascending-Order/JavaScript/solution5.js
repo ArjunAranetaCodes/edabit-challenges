8762967a5365f5db7b131a7696f4e9544043edc8
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-11 05:00:00 +0800

    Edabit Challenges

--- Easy/Sort-Numbers-in-Ascending-Order/JavaScript/solution5.js
+++ Easy/Sort-Numbers-in-Ascending-Order/JavaScript/solution5.js
@@ -1,13 +1,3 @@
-function highLow(str) {
-	var arrNums = str.split(" ");
-	var newArr = []
-	for(var x = 0; x < arrNums.length; x++){
-		newArr.push(parseInt(arrNums[x]))
-	}
-	if(str.length < 2){
-		return [newArr[0], newArr[0]]
-	}
-	newArr.sort(function(a, b){return a-b});
-	
-	return newArr[newArr.length - 1].toString() + " " + newArr[0].toString()
+function sortNumsAscending(arr) {
+  return arr ? arr.sort(function(a, b){ return a - b; }) : [];
 }
