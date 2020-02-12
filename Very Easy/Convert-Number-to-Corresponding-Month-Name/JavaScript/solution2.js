5a5c1e12f57146cb6790b1e036b0441be5443ef4
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-13 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Convert-Number-to-Corresponding-Month-Name/JavaScript/solution2.js
+++ Very Easy/Convert-Number-to-Corresponding-Month-Name/JavaScript/solution2.js
@@ -1,5 +1,17 @@
-function getFillings(sandwich) {
-	sandwich.pop();
-	sandwich.shift();
-	return sandwich;
+function monthName(num) {
+	const mapping = {
+		1: 'January',
+		2: 'February',
+		3: 'March',
+		4: 'April',
+		5: 'May',
+		6: 'June',
+		7: 'July',
+		8: 'August', 
+		9: 'September', 
+		10:	'October', 
+		11:	'November', 
+		12:	'December'
+	};
+	return mapping[num];
 }
