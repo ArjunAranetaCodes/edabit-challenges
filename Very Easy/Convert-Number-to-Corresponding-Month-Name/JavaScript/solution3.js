04937713d4abb88a7f1106d120eb8b34efa8820f
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-13 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Convert-Number-to-Corresponding-Month-Name/JavaScript/solution3.js
+++ Very Easy/Convert-Number-to-Corresponding-Month-Name/JavaScript/solution3.js
@@ -1,1 +1,5 @@
-const getFillings = sandwich => sandwich.filter(ingredient => ingredient !== "bread")
+function monthName(num) {
+	let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	let month = monthArray[num -1]
+	return month
+	}
