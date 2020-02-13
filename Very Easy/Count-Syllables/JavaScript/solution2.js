e56006c594fd41499dbc3b2e67d92ac557cfc444
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-14 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Count-Syllables/JavaScript/solution2.js
+++ Very Easy/Count-Syllables/JavaScript/solution2.js
@@ -1,17 +1,1 @@
-function monthName(num) {
-	const mapping = {
-		1: 'January',
-		2: 'February',
-		3: 'March',
-		4: 'April',
-		5: 'May',
-		6: 'June',
-		7: 'July',
-		8: 'August', 
-		9: 'September', 
-		10:	'October', 
-		11:	'November', 
-		12:	'December'
-	};
-	return mapping[num];
-}
+const numberSyllables = (word) => word.split('-').length;
