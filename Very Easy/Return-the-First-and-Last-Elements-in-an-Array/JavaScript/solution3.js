27f5416fe48012e7bb539ef3c9efc9b7f8cf9c1b
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-15 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Return-the-First-and-Last-Elements-in-an-Array/JavaScript/solution3.js
+++ Very Easy/Return-the-First-and-Last-Elements-in-an-Array/JavaScript/solution3.js
@@ -1,11 +1,1 @@
-function numberSyllables(word) {
-  let count = 1;
-  for (let i = 0; i < word.length; i++) {
-
-    if (word[i] === '-') {
-      count += 1
-    }
-
-  }
-  return count
-}
+const firstLast = ([first, ...rest]) => [first, rest.pop()];
