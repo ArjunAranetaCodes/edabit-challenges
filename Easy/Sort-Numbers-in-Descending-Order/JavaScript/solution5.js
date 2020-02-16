a430c3e27bf578d80352a84bc2db777b499d7665
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-17 05:00:00 +0800

    Edabit Challenges

--- Easy/Sort-Numbers-in-Descending-Order/JavaScript/solution5.js
+++ Easy/Sort-Numbers-in-Descending-Order/JavaScript/solution5.js
@@ -1,9 +1,3 @@
-function isPrefix(word, prefix) {
-	prefix = prefix.substring(0,(prefix.length - 1));
-	return prefix == (word.substring(0,prefix.length)) ? true : false ;
-}
-
-function isSuffix(word, suffix) {
-	suffix = suffix.substring(1,suffix.length);
-	return suffix == (word.substr(0-suffix.length)) ? true : false ;	
+function sortDescending(num) {
+	return parseInt(num.toString().split('').sort().reverse().join(''));
 }
