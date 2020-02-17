acca567be811f9c3ff4edbb15772da3f824907de
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-18 05:00:00 +0800

    Edabit Challenges

--- Medium/Algebra-Sequence-Boxes/JavaScript/solution5.js
+++ Medium/Algebra-Sequence-Boxes/JavaScript/solution5.js
@@ -1,7 +1,11 @@
-function checkEquals(arr1, arr2) {
-	if (arr1.join() === arr2.join()) {
-  	return true 
-  } else {
-  	return false
-  }
+function boxSeq(step) {
+	var totalBoxes = 0
+	for(var x = 1; x <= step; x++){
+		if(x % 2 == 0){
+			totalBoxes = totalBoxes - 1
+		}else{
+			totalBoxes = totalBoxes + 3
+		}
+	}
+	return totalBoxes
 }
