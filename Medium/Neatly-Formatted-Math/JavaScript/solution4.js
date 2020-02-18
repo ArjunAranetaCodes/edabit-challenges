e54a0952640a3faa99a137192bef884802fd7559
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-19 05:00:00 +0800

    Edabit Challenges

--- Medium/Neatly-Formatted-Math/JavaScript/solution4.js
+++ Medium/Neatly-Formatted-Math/JavaScript/solution4.js
@@ -1,1 +1,21 @@
-const boxSeq=s=>s%2?s+2:s
+function formatMath(expr) {
+	var split = expr.split(' ');
+	var res = 0;
+	var num1 = parseInt(split[0]);
+	var num2 = parseInt(split[2]);
+	switch(split[1]) {
+		case '+':
+			res = num1 + num2;
+			break;
+		case '-':
+			res = num1 - num2;
+			break;
+		case 'x':
+			res = num1 * num2;
+			break;
+		case '/':
+			res = num1 / num2;
+			break;
+	}
+	return(`${split[0]} ${split[1]} ${split[2]} = ${res}`);
+}
