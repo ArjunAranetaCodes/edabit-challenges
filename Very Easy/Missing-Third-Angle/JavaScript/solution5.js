1624077a7399da2b750b95aa984904355193ace9
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-20 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Missing-Third-Angle/JavaScript/solution5.js
+++ Very Easy/Missing-Third-Angle/JavaScript/solution5.js
@@ -1,1 +1,10 @@
-const firstLast = arr => [arr.shift(), arr.pop()];
+function missingAngle(angle1, angle2) {
+	var angle = 180 - (angle1 + angle2)
+	if(angle < 90){
+		return "acute"
+	}else if(angle > 90){
+		return "obtuse"
+	}else if(angle == 90){
+		return "right"
+	}
+}
