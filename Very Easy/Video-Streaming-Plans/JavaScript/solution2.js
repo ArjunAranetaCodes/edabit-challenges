c15afd2e26cc054841101fa237d1ede13fe2c7b7
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-21 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Video-Streaming-Plans/JavaScript/solution2.js
+++ Very Easy/Video-Streaming-Plans/JavaScript/solution2.js
@@ -1,5 +1,20 @@
-function missingAngle(angle1, angle2) {
-	return angle1 + angle2 < 90 ? 'obtuse'
-		: angle1 + angle2 > 90 ? 'acute'
-		: 'right';
+class BasicPlan {
+	static numOfDevices = 1;
+	static canStream = true;
+	static canDownload = true;
+	static hasSD = true;
+	static hasHD = false;
+	static hasUHD = false;
+	static price = '$8.99';
+}
+class StandardPlan extends BasicPlan {
+	static numOfDevices = 2;
+	static hasHD = true;
+	static hasUHD = false;
+	static price = '$12.99';
+}
+class PremiumPlan extends StandardPlan {
+	static numOfDevices = 4;
+	static hasUHD = true;
+	static price = '$15.99';
 }
