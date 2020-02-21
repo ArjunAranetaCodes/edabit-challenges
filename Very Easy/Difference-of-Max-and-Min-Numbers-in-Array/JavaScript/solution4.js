f19fc4480bdd68a6e0cdf9217c5237616f9f03b3
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-22 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Difference-of-Max-and-Min-Numbers-in-Array/JavaScript/solution4.js
+++ Very Easy/Difference-of-Max-and-Min-Numbers-in-Array/JavaScript/solution4.js
@@ -1,21 +1,3 @@
-class BasicPlan {
-	static canStream = true;
-	static canDownload = true;
-	static numOfDevices = 1;
-	static hasSD = true;
-	static hasHD = false;
-	static hasUHD = false;
-	static price = '$8.99';
-}
-
-class StandardPlan extends BasicPlan {
-	static numOfDevices = 2;
-	static hasHD = true;
-	static price = '$12.99';
-}
-
-class PremiumPlan extends StandardPlan {
-	static numOfDevices = 4;
-	static hasUHD = true;
-	static price = '$15.99';
+function diffMaxMin(arr) {
+	return Math.max(...arr) - Math.min(...arr);
 }
