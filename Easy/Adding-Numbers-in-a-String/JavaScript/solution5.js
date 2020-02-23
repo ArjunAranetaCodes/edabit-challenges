3687e707eabb9320c964b49065535ba5d4047a24
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-24 05:00:00 +0800

    Edabit Challenges

--- Easy/Adding-Numbers-in-a-String/JavaScript/solution5.js
+++ Easy/Adding-Numbers-in-a-String/JavaScript/solution5.js
@@ -1,5 +1,8 @@
-function leftDigit(num) {
-	var filtered = parseInt(num.split("").filter(elem => !isNaN(elem)).shift());
-	
-	return filtered;
+function maximumScore(tileHand) {
+	var sum = 0
+	for(var x = 0; x < tileHand.length; x++){
+		var obj = tileHand[x]
+		sum = sum + obj.score
+	}
+	return sum
 }
