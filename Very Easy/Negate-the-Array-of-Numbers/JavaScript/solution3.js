67f6964a48a8748c6ebd153fb0ffb3f3ed3d7700
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-27 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Negate-the-Array-of-Numbers/JavaScript/solution3.js
+++ Very Easy/Negate-the-Array-of-Numbers/JavaScript/solution3.js
@@ -1,13 +1,6 @@
-function diffMaxMin(arr) {
-	var largest = arr[0];
-	var smallest = arr[0];
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] > largest) {
-			largest = arr[i]
-		}
-		if (arr[i] < smallest) {
-			smallest = arr[i];
-		}
+function negate(arr) {
+	for(let i = 0; i < arr.length; i++) {
+		arr[i] = -arr[i];
 	}
-	return largest - smallest;
+	return arr;
 }
