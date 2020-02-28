e13877b235d78159f4f93718b644fc5019862b08
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-29 05:00:00 +0800

    Edabit Challenges

--- Medium/Reverse-the-Number/JavaScript/solution3.js
+++ Medium/Reverse-the-Number/JavaScript/solution3.js
@@ -1,3 +1,10 @@
-function concat(...args) {
-	return [].concat(...args);
+function rev(n) {
+	let arr=n.toString().split('')
+	let ans= arr.reverse()
+	
+	if(ans[ans.length-1]!="-"){return ans.join('')}
+	else{
+	 ans.pop()
+		return ans.join('')
+	}
 }
