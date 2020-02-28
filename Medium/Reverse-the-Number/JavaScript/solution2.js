ed42e772c8efe92e0aeae326e09b5f098dc501ca
Arjun Araneta <arjunaraneta@gmail.com>
2020-02-29 05:00:00 +0800

    Edabit Challenges

--- Medium/Reverse-the-Number/JavaScript/solution2.js
+++ Medium/Reverse-the-Number/JavaScript/solution2.js
@@ -1,1 +1,10 @@
-const concat = (...a) => a.flat(Infinity);
+function rev(n) {
+	var strNum = ""
+	if(n < 0) n = n * -1
+	while(n > 0){
+		var rem = n % 10
+		strNum = strNum + rem.toString()
+		n = parseInt(n / 10)
+	}
+	return strNum
+}
