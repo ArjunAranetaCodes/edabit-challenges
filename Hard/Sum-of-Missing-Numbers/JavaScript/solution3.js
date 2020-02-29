fcd879cc40b3d7761e5dc56328e68cd9b4f8a399
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-01 05:00:00 +0800

    Edabit Challenges

--- Hard/Sum-of-Missing-Numbers/JavaScript/solution3.js
+++ Hard/Sum-of-Missing-Numbers/JavaScript/solution3.js
@@ -1,3 +1,3 @@
-function findBrokenKeys(str1, str2) {
-	return [...new Set([...str1].filter((val, i) => val !== str2[i]))]
-}
+const sumAB = (a, b) => ((b - a + 1) * (a + b)) >> 1;
+const sum = ar => ar.reduce((a,v) => a + v, 0);
+const sumMissingNumbers = arr => sumAB(Math.min(...arr), Math.max(...arr)) - sum(arr);
