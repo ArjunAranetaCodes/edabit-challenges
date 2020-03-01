d4a18d73e7d7d6651c254c86dd54124b11935fb9
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-02 05:00:00 +0800

    Edabit Challenges

--- Hard/Reverse-the-Odd-Length-Words/JavaScript/solution4.js
+++ Hard/Reverse-the-Odd-Length-Words/JavaScript/solution4.js
@@ -1,9 +1,1 @@
-function sumMissingNumbers(arr) {
-	var sum = 0;
-	for(var x = Math.min(...arr); x <= Math.max(...arr); x++){
-		if(!arr.includes(x)){
-			sum = sum + x
-		}
-	}
-	return sum
-}
+reverseOdd = (str)=> str.split(' ').map((a)=> {return a.length%2 ? a.split('').reverse().join('') : a} ).join(' ')
