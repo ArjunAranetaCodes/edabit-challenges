acd0ce5bd2d2a31324e448ee2d8cb93e1740357f
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-05 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Recursion-Sum/JavaScript/solution4.js
+++ Very Easy/Recursion-Sum/JavaScript/solution4.js
@@ -1,1 +1,7 @@
-const difference = (nums) => Math.max(...nums) - Math.min(...nums);
+const sum = function(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+        arr.push(i);
+    }
+    return arr.reduce((acc, cur) => acc + cur);
+};
