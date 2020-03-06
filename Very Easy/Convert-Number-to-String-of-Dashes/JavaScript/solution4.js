a6118cc0cec4f3c88140cc55039192f169611d18
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-07 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Convert-Number-to-String-of-Dashes/JavaScript/solution4.js
+++ Very Easy/Convert-Number-to-String-of-Dashes/JavaScript/solution4.js
@@ -1,9 +1,3 @@
-function noOdds(arr) {
-  var result = [];
-  for(var i = 0; i < arr.length; i++) {
-    if(arr[i]%2 == 0) {
-      result.push(arr[i]);
-    }
-  }
-  return result;
+function Go(num) {
+	return "-".repeat(num);
 }
