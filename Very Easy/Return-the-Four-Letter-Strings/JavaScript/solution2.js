a60e25d7efc8239623ce9ec0eeb75bb77d333cd6
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-08 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Return-the-Four-Letter-Strings/JavaScript/solution2.js
+++ Very Easy/Return-the-Four-Letter-Strings/JavaScript/solution2.js
@@ -1,1 +1,9 @@
-const Go = num => '-'.repeat(num);
+function isFourLetters(arr) {
+  var result = [];
+  for(var i=0; i < arr.length; i++){
+    if (arr[i].length == 4){
+      result.push(arr[i]);
+    }
+	}
+  return result;
+}
