4b3c9abd0e7255a75b1102bb1c4cb722ea20f1c6
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-18 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Multiply-by-Length/JavaScript/solution3.js
+++ Very Easy/Multiply-by-Length/JavaScript/solution3.js
@@ -1,8 +1,1 @@
-function replaceVowels(str, ch) {
-	const vowels = ['a', 'e', 'i', 'o', 'u'];
-	
-	const strArry = [...str].map((char) => {
-		return vowels.includes(char) ? ch : char;
-	});
-	return strArry.join('');
-}
+multiplyByLength = arr => arr.map(x=>x*arr.length)
