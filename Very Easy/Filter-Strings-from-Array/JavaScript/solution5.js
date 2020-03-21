1b556dba97c215c0d49070e97481c70c778a1c78
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-22 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Filter-Strings-from-Array/JavaScript/solution5.js
+++ Very Easy/Filter-Strings-from-Array/JavaScript/solution5.js
@@ -1,1 +1,12 @@
-const REGEXP = /\w/g
+function filterArray(arr){
+	result = arr.filter(removeStrings);
+	
+	function removeStrings(a){
+		if (typeof (a) === 'number' && a % 1 == 0){
+			return true;
+		}else {
+			return false;
+		}
+	}
+	return result;
+}
