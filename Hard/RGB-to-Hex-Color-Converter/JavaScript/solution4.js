cc43eda22640b98ef1d408f2202471c9c9972ca3
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-23 05:00:00 +0800

    Edabit Challenges

--- Hard/RGB-to-Hex-Color-Converter/JavaScript/solution4.js
+++ Hard/RGB-to-Hex-Color-Converter/JavaScript/solution4.js
@@ -1,5 +1,13 @@
-const pentagonal = num => {
-	if (num === 1) return 1;
-	
-	return 5 * (num - 1) + pentagonal(num - 1);
+function rgbToHex(col) {
+	var rgbValues = col.substring(4, col.length - 1)
+	var arrValues = rgbValues.split(",")
+	var hexStr = ""
+	for(var x = 0; x < arrValues.length; x++){
+		var hex = Number(arrValues[x]).toString(16);
+		if (hex.length < 2) {
+				 hex = "0" + hex;
+		}
+		hexStr = hexStr + hex
+	}
+	return "#" + hexStr
 }
