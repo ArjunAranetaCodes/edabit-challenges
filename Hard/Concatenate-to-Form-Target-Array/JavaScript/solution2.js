3f7f24409e1ea0211e93f3a5b7518940731defba
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-23 05:00:00 +0800

    Edabit Challenges

--- Hard/Concatenate-to-Form-Target-Array/JavaScript/solution2.js
+++ Hard/Concatenate-to-Form-Target-Array/JavaScript/solution2.js
@@ -1,9 +1,2 @@
-function rgbToHex(col) {
-	col = col.replace('rgb(', '').replace(')', '').split(',');
-	var r = parseInt(col[0], 10).toString(16);
-	var g = parseInt(col[1], 10).toString(16);
-	var b = parseInt(col[2], 10).toString(16);
-	r = r.length == 1 ? '0' + r : r; g = g.length == 1 ? '0' + g : g; b = b.length == 1 ? '0' + b : b;
-	var colHex = '#' + r + g + b;
-	return colHex;	
-}
+const canConcatenate = (a, t) =>
+ a.flat().reduce((a,b)=> a+b) === t.reduce((a,b)=>a+b);
