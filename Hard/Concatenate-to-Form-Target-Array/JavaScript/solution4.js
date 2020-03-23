4346b7a813e265e38f263b88a374fbee16385c68
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-24 05:00:00 +0800

    Edabit Challenges

--- Hard/Concatenate-to-Form-Target-Array/JavaScript/solution4.js
+++ Hard/Concatenate-to-Form-Target-Array/JavaScript/solution4.js
@@ -1,13 +1,10 @@
-function rgbToHex(col) {
-	var rgbValues = col.substring(4, col.length - 1)
-	var arrValues = rgbValues.split(",")
-	var hexStr = ""
-	for(var x = 0; x < arrValues.length; x++){
-		var hex = Number(arrValues[x]).toString(16);
-		if (hex.length < 2) {
-				 hex = "0" + hex;
-		}
-		hexStr = hexStr + hex
+function canConcatenate(arr, target) {
+	var newArr = arr.flat(Infinity)
+	newArr.sort(function(a, b){return a-b});
+	target.sort(function(a, b){return a-b});
+	if(JSON.stringify(newArr) === JSON.stringify(target)){
+		return true
+	}else{
+		return false
 	}
-	return "#" + hexStr
 }
