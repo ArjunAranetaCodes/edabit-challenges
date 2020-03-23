52136dcda86119f59fb20569985cb3460ca742da
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-24 05:00:00 +0800

    Edabit Challenges

--- Hard/Concatenate-to-Form-Target-Array/JavaScript/solution5.js
+++ Hard/Concatenate-to-Form-Target-Array/JavaScript/solution5.js
@@ -1,1 +1,9 @@
-const rgbToHex = col => '#'+col.match(/\d+/g).map(v => (+v).toString(16).padStart(2,'0')).join('');
+function canConcatenate(array, target) {
+	var mergedArray = array.reduce(function(a,b){return a.concat(b);});
+	mergedArray = mergedArray.sort(function(a,b){return a-b;});
+	target = target.sort(function(a,b){return a-b;});
+	for(var ndx = 0; ndx < mergedArray.length; ndx++){
+		if(mergedArray[ndx] != target[ndx]) return false;
+	}
+	return true;
+}
