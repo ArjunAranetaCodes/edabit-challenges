b295825481137f6cc51c5fde724a768359f08372
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-25 05:00:00 +0800

    Edabit Challenges

--- Hard/Numbers-in-Strings/JavaScript/solution5.js
+++ Hard/Numbers-in-Strings/JavaScript/solution5.js
@@ -1,9 +1,3 @@
-function canConcatenate(array, target) {
-	var mergedArray = array.reduce(function(a,b){return a.concat(b);});
-	mergedArray = mergedArray.sort(function(a,b){return a-b;});
-	target = target.sort(function(a,b){return a-b;});
-	for(var ndx = 0; ndx < mergedArray.length; ndx++){
-		if(mergedArray[ndx] != target[ndx]) return false;
-	}
-	return true;
+function numInStr(arr) {
+	return arr.filter(x => /[0-9]/.test(x));
 }
