7a3f6b6e3074556c330be96c372cd514a6a597b1
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-25 05:00:00 +0800

    Edabit Challenges

--- Hard/Numbers-in-Strings/JavaScript/solution2.js
+++ Hard/Numbers-in-Strings/JavaScript/solution2.js
@@ -1,2 +1,9 @@
-const canConcatenate = (a, t) =>
- a.flat().reduce((a,b)=> a+b) === t.reduce((a,b)=>a+b);
+function numInStr(arr) {
+	return arr.filter(item => {
+		for (char of item){
+			if (char.charCodeAt(0)>47 && char.charCodeAt(0)<58){
+				return item;
+			}
+		}
+	})
+}
