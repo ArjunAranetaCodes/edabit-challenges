53542a07badcd00ce451c442b24c98c29e6dc499
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-25 05:00:00 +0800

    Edabit Challenges

--- Hard/Numbers-in-Strings/JavaScript/solution4.js
+++ Hard/Numbers-in-Strings/JavaScript/solution4.js
@@ -1,10 +1,1 @@
-function canConcatenate(arr, target) {
-	var newArr = arr.flat(Infinity)
-	newArr.sort(function(a, b){return a-b});
-	target.sort(function(a, b){return a-b});
-	if(JSON.stringify(newArr) === JSON.stringify(target)){
-		return true
-	}else{
-		return false
-	}
-}
+const numInStr = arr => arr.filter(str => /\d/.test(str));
