8e1bd7f6ff2d46b58d4cebd247b7862430c1ad62
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-26 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Semantic-Versioning/JavaScript/solution4.js
+++ Very Easy/Semantic-Versioning/JavaScript/solution4.js
@@ -1,3 +1,11 @@
-function filterArray(arr){
-	return arr.filter(numbers => typeof(numbers) === 'number' && Number.isInteger(numbers));
+function retrieveMajor(semver) {
+		return semver.charAt(0)
+}
+
+function retrieveMinor(semver) {
+		return semver.charAt(2)
+}
+
+function retrievePatch(semver) {
+		return semver.charAt(4)
 }
