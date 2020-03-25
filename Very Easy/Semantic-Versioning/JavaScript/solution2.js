b434606317678433fde32ed650097903988ff2eb
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-26 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Semantic-Versioning/JavaScript/solution2.js
+++ Very Easy/Semantic-Versioning/JavaScript/solution2.js
@@ -1,1 +1,11 @@
-let filterArray = a => a.filter(x=> x % 1 == 0);
+function retrieveMajor(semver) {
+	return semver.split(".")[0];
+}
+
+function retrieveMinor(semver) {
+	return semver.split(".")[1];
+}
+
+function retrievePatch(semver) {
+	return semver.split(".")[2];
+}
