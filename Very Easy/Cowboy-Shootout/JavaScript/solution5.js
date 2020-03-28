31da5c0c7a925010da6efde15bc000bdc36c291f
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-29 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Cowboy-Shootout/JavaScript/solution5.js
+++ Very Easy/Cowboy-Shootout/JavaScript/solution5.js
@@ -1,6 +1,9 @@
-function FizzBuzz(num) {
-	return num % 15 === 0 ? 'FizzBuzz' 
-	     : num % 5 === 0 ? 'Buzz' 
-	     : num % 3 === 0 ? 'Fizz' 
-	     : String(num);
+function rogerShots(arr) {
+	var sum = 0.0
+	for(var x = 0; x < arr.length; x++){
+		if(arr[x] === "Bang!" || arr[x] === "BangBang!"){
+			sum = sum + .5
+		}
+	}
+	return sum
 }
