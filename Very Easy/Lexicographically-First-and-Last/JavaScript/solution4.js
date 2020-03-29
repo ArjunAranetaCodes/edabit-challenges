69c275cd45ac0d580895323a7bd4808458848516
Arjun Araneta <arjunaraneta@gmail.com>
2020-03-30 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Lexicographically-First-and-Last/JavaScript/solution4.js
+++ Very Easy/Lexicographically-First-and-Last/JavaScript/solution4.js
@@ -1,16 +1,6 @@
-function rogerShots(arr) {
-	let bottlesShoot = 0
-	let time = 0
-	for (let shot of arr) {
-		if (shot === "Bang!") {
-			bottlesShoot++
-			time+=0.5
-		}
-		if (shot === "BangBang!"){
-			bottlesShoot+=2
-			time+=0.5
-		}
-		if (bottlesShoot >= 6) break
-	}
-	return time
+function firstAndLast(s) {
+	let x = s.split('');
+	let first = x.sort().join('');
+	let last = x.sort().reverse().join('');
+	return [first, last];
 }
