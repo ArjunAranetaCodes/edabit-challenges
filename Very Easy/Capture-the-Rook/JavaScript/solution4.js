d8f848b3286b86da1bdf74975d8f198de8754085
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-03 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Capture-the-Rook/JavaScript/solution4.js
+++ Very Easy/Capture-the-Rook/JavaScript/solution4.js
@@ -1,7 +1,5 @@
-function checkEquals(arr1, arr2) {
-	if (arr1.join() === arr2.join()) {
-  	return true
- 	} else {
-  	return false
- 	}
+function canCapture([yourRook, opponentsRook]) {
+	[ yourRow , yourCol] =  [yourRook[0] , yourRook[1]];
+	[ opponentsRow , opponentsCol] = [opponentsRook[0] , opponentsRook[1]];
+	return (yourRow == opponentsRow) || (yourCol == opponentsCol);
 }
