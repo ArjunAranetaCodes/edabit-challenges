532b55a86eb393a9e3453c41854c5c355e25a9ba
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-04 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Repeat-the-Same-Item-Multiple-Times/JavaScript/solution4.js
+++ Very Easy/Repeat-the-Same-Item-Multiple-Times/JavaScript/solution4.js
@@ -1,5 +1,1 @@
-function canCapture([yourRook, opponentsRook]) {
-	[ yourRow , yourCol] =  [yourRook[0] , yourRook[1]];
-	[ opponentsRow , opponentsCol] = [opponentsRook[0] , opponentsRook[1]];
-	return (yourRow == opponentsRow) || (yourCol == opponentsCol);
-}
+const repeat = (item, times) => Array(times).fill(item);
