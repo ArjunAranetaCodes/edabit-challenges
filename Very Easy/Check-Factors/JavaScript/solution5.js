afa60124ef565846d8e3ffadd14ef76bcf5b875b
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-14 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Check-Factors/JavaScript/solution5.js
+++ Very Easy/Check-Factors/JavaScript/solution5.js
@@ -1,1 +1,8 @@
-isTruthy = (v) => v ? 1 : 0
+function checkFactors(factors, num) {
+	for (let i = 0; i < factors.length; i++) {
+		if (num % factors[i] !== 0) {
+			return false;
+		}
+	}
+	return true;
+}
