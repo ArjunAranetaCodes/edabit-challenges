18849ac4c8b865141e8273a79352873d99361b80
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-17 05:00:00 +0800

    Edabit Challenges

--- Very Easy/X-and-Y-Coordinates/JavaScript/solution5.js
+++ Very Easy/X-and-Y-Coordinates/JavaScript/solution5.js
@@ -1,9 +1,3 @@
-function unlucky13(nums) {
-	var arr = [];
-for(i in nums){
-	if(nums[i]%13 != 0){
-		arr.push(nums[i]);
-	}
-}
-	return arr;
+function convertCartesian(x, y) {
+	return x.map((n, i) => [n, y[i]]);
 }
