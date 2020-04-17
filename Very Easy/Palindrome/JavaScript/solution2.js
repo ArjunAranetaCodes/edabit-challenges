fabcd198fbef794d4b565be4ee88ea6389c7eb69
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-18 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Palindrome/JavaScript/solution2.js
+++ Very Easy/Palindrome/JavaScript/solution2.js
@@ -1,9 +1,3 @@
-function convertCartesian(x, y) {
-	let coords = [];
-	for (let i=0; i < x.length; i++) {
-		let coord = [];
-		coord.push(x[i], y[i])
-		coords.push(coord);
-	}
-	return coords;
+function checkPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
