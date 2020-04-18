e6664f4813bd95de6c648062440711a57f397d36
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-19 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Hurdle-Jump/JavaScript/solution3.js
+++ Very Easy/Hurdle-Jump/JavaScript/solution3.js
@@ -1,8 +1,1 @@
-function checkPalindrome(str) {
-  for (i = 0; i < str.length; i++) {
-		if (str[i] !== str[str.length - 1 - i]) {
-      return false;
-    }
-  }
-return true;
-}
+const hurdleJump = (arr, num) => Math.max(...arr) <= num
