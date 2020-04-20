e7e1b3902b6e7b378c7488ea74d5f5efb44a2a70
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-21 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Find-the-Bug-Checking-Even-Numbers/JavaScript/solution4.js
+++ Very Easy/Find-the-Bug-Checking-Even-Numbers/JavaScript/solution4.js
@@ -1,4 +1,5 @@
-function howManyTimes(num) {
-  let str = "a";
-  return `Ed${str.repeat(num)}bit`;
+// Fix this incorrect code!
+function checkAllEven(arr) {
+	const isAlleven = (currentValue) => currentValue % 2 === 0;
+  return arr.every(isAlleven)
 }
