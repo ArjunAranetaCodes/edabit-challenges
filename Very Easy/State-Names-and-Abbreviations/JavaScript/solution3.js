375ca96482b5ae7ce3517145787384377096d773
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-24 05:00:00 +0800

    Edabit Challenges

--- Very Easy/State-Names-and-Abbreviations/JavaScript/solution3.js
+++ Very Easy/State-Names-and-Abbreviations/JavaScript/solution3.js
@@ -1,6 +1,7 @@
-function isEmpty(obj) {
-	for(var key in obj){
-		return false
+function filterStateNames(arr, type) {
+	if(type == 'abb') {
+		return arr.filter(el => el.length <= 2);
+	}else if(type == 'full') {
+		return arr.filter(el => el.length > 2);
 	}
-	return true
 }
