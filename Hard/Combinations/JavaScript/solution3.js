5b515d4892d7a246ecfd6f43e5005b2c597a3cd6
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-26 05:00:00 +0800

    Edabit Challenges

--- Hard/Combinations/JavaScript/solution3.js
+++ Hard/Combinations/JavaScript/solution3.js
@@ -1,9 +1,4 @@
-function numInStr(arr) {
-	return arr.filter(arrItem => {
-		let valid = false;
-		arrItem.split("").forEach(char => {
-			if(Number.isInteger(parseInt(char))) valid = true
-		})
-		if(valid) return arrItem
-	})
+function combinations(items) {
+	let args = Array.prototype.slice.call(arguments);
+	return args.filter(x => x > 0).reduce((a, b) => a * b, 1);
 }
