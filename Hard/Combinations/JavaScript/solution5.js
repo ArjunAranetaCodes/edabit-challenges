737bd107bf11233ba3784e31d5c04a8c3226d95e
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-26 05:00:00 +0800

    Edabit Challenges

--- Hard/Combinations/JavaScript/solution5.js
+++ Hard/Combinations/JavaScript/solution5.js
@@ -1,3 +1,10 @@
-function numInStr(arr) {
-	return arr.filter(x => /[0-9]/.test(x));
+function combinations(items) {
+	let args = Array.from(arguments);
+	let perms = 1;
+	for(var x = 0; x < args.length; x++){
+		if(args[x] > 0){
+			perms = perms * args[x]
+		}
+	}
+	return perms
 }
