223aeab4e34919a3d6ea1a408be05e557668c7e8
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-27 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Averages-Methods-Extending-the-Math-Object/JavaScript/solution2.js
+++ Very Hard/Averages-Methods-Extending-the-Math-Object/JavaScript/solution2.js
@@ -1,13 +1,22 @@
-function happy(n) {
-  if (n === 1) return true;
-  if (n >= 10) {
-    return happy(
-      n
-        .toString()
-        .split("")
-        .reduce((a, c) => Math.pow(c, 2) + a, 0)
-    );
-  } else {
-    return false;
-  }
-}
+const sum = arr => arr.reduce((total, num) => total + num, 0);
+
+const product = arr => arr.reduce((total, num) => total * num, 1);
+
+const mean = arr => sum(arr) / arr.length;
+
+const withPrecision = (num, prec) =>
+  typeof prec === 'number' ? Number(num.toFixed(prec)) : num;
+
+Math.avg = (arr, prec) => withPrecision(mean(arr), prec);
+
+Math.qAvg = (arr, prec) =>
+  withPrecision(Math.sqrt(mean(arr.map(num => num ** 2))), prec);
+
+Math.hAvg = (arr, prec) =>
+  withPrecision(1 / mean(arr.map(num => 1 / num)), prec);
+
+Math.gAvg = (arr, prec) =>
+  withPrecision(Math.nthRt(product(arr), arr.length), prec);
+
+Math.nthRt = (num, root, prec) =>
+  withPrecision(Math.exp((1 / root) * Math.log(num)), prec);
