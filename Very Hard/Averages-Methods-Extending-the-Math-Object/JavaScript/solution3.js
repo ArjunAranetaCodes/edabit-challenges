31030522856e1e4a6cdaf8fa43c31681963a491d
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-27 05:00:00 +0800

    Edabit Challenges

--- Very Hard/Averages-Methods-Extending-the-Math-Object/JavaScript/solution3.js
+++ Very Hard/Averages-Methods-Extending-the-Math-Object/JavaScript/solution3.js
@@ -1,9 +1,19 @@
-const sum = arr => arr.reduce((total, num) => total + num, 0);
+Math.avg = function(arr, prec = 20) {
+	return +(arr.reduce((a, c) => a + c, 0) / arr.length).toFixed(prec);
+}
 
-const getDigits = num => String(num).split('').map(Number);
+Math.qAvg = function(arr, prec = 20) {
+	return +Math.sqrt(arr.reduce((a, c) => a + c*c, 0) / arr.length).toFixed(prec);
+}
 
-const happy = num => {
-  if (num === 1) return true;
-  if (num === 4) return false;
-  return happy(sum(getDigits(num).map(digit => Math.pow(digit, 2))));
-};
+Math.hAvg = function(arr, prec = 20) {
+	return +(1/arr.reduce((a, c) => a + 1/(c*arr.length), 0)).toFixed(prec);
+}
+
+Math.gAvg = function(arr, prec = 20) {
+	return Math.nthRt(arr.reduce((a, c) => a * c, 1), arr.length, prec);
+}
+
+Math.nthRt = function(num, root, prec = 20) {
+	return +Math.exp((1/root) * Math.log(num)).toFixed(prec);
+}
