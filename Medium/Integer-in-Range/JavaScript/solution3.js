7d026787d6de727eaa67ebf720a7242c07c5632b
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-28 05:00:00 +0800

    Edabit Challenges

--- Medium/Integer-in-Range/JavaScript/solution3.js
+++ Medium/Integer-in-Range/JavaScript/solution3.js
@@ -1,10 +1,3 @@
-function rev(n) {
-	let arr=n.toString().split('')
-	let ans= arr.reverse()
-	
-	if(ans[ans.length-1]!="-"){return ans.join('')}
-	else{
-	 ans.pop()
-		return ans.join('')
-	}
+function intWithinBounds(n, lower, upper) {
+	return Number.isInteger(n) ? n>=lower && n<upper : false
 }
