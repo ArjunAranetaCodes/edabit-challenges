881855005f41260fdc3aa20d53d03c1a9c905881
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-28 05:00:00 +0800

    Edabit Challenges

--- Medium/Integer-in-Range/JavaScript/solution2.js
+++ Medium/Integer-in-Range/JavaScript/solution2.js
@@ -1,10 +1,3 @@
-function rev(n) {
-	var strNum = ""
-	if(n < 0) n = n * -1
-	while(n > 0){
-		var rem = n % 10
-		strNum = strNum + rem.toString()
-		n = parseInt(n / 10)
-	}
-	return strNum
+function intWithinBounds(n, lower, upper) {
+	return lower <= n && n < upper && Number.isSafeInteger(n);
 }
