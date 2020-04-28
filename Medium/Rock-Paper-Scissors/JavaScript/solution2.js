9732850dc4f83366bcd600d4b89a1724618d9700
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-29 05:00:00 +0800

    Edabit Challenges

--- Medium/Rock-Paper-Scissors/JavaScript/solution2.js
+++ Medium/Rock-Paper-Scissors/JavaScript/solution2.js
@@ -1,3 +1,4 @@
-function intWithinBounds(n, lower, upper) {
-	return lower <= n && n < upper && Number.isSafeInteger(n);
-}
+const rps = (p1, p2) => {
+	dic = {Rock: 'Scissors', Scissors: 'Paper', Paper: 'Rock'};
+	return p1==p2? "It's a draw" : `The winner is p${2-(dic[p1]==p2)}`;
+};
