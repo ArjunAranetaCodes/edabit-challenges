0cd5fe5d34cb4c6b10a9e6cc75a38439cc9edd38
Arjun Araneta <arjunaraneta@gmail.com>
2020-04-29 05:00:00 +0800

    Edabit Challenges

--- Medium/Rock-Paper-Scissors/JavaScript/solution4.js
+++ Medium/Rock-Paper-Scissors/JavaScript/solution4.js
@@ -1,3 +1,4 @@
-function intWithinBounds(n, lower, upper) {
-	return Number.isInteger(n) ? n >= lower && n < upper : false
+function rps(p1, p2) {
+	const wins = ['RockScissors', 'PaperRock', 'ScissorsPaper'];
+  	return p1 === p2 ? "It's a draw" : `The winner is ${wins.includes(p1 + p2) ? 'p1' : 'p2'}`; 
 }
