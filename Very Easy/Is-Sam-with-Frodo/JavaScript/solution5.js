70d34f108486183c96ca5d5512b7ca9f0cb6bff9
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-03 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Is-Sam-with-Frodo/JavaScript/solution5.js
+++ Very Easy/Is-Sam-with-Frodo/JavaScript/solution5.js
@@ -1,5 +1,2 @@
-function nextElement(arr) {
-  const arr1 = arr[arr.length - 1];
-  const arr2 = arr[arr.length - 2];
-  return (arr1 - arr2) + arr1;
-}
+const middleEarth = a => 
+ Math.abs(a.indexOf('Sam') - a.indexOf('Frodo')) <= 1;
