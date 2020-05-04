67855f03d38ea1179ebc8d33825c1c357ed19352
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-05 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Array-from-Comma-Delimited-String/JavaScript/solution3.js
+++ Very Easy/Array-from-Comma-Delimited-String/JavaScript/solution3.js
@@ -1,7 +1,1 @@
-function isInOrder(str) {
-	str = str.split("");
-	for (let i = 0; i < str.length - 1; i++) {
-		if (str[i].charCodeAt(0) > str[i+1].charCodeAt(0)) return false;
-	}
-	return true;
-}
+const toArray = str => str.split(', ').filter(Boolean);
