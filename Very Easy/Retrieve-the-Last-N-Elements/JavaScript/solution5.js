7a95b63eb70931dc3b04e6005d9ea43cde0fffc8
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-07 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Retrieve-the-Last-N-Elements/JavaScript/solution5.js
+++ Very Easy/Retrieve-the-Last-N-Elements/JavaScript/solution5.js
@@ -1,1 +1,12 @@
-let removeFirstLast = s => s.slice(1, -1)||s;
+function last(a, n) {
+	if (a.length >= n) {
+		let arr = [];
+		for (let i = n; i > 0; i--) {
+			arr.push(a[a.length - i]);
+		}
+		return arr;
+	}
+	else {
+		return "invalid"
+	}
+}
