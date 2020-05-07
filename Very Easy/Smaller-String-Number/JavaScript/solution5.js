5fd5809543bdbab1c52aef60c7556b2749a4a90f
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-08 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Smaller-String-Number/JavaScript/solution5.js
+++ Very Easy/Smaller-String-Number/JavaScript/solution5.js
@@ -1,12 +1,5 @@
-function last(a, n) {
-	if (a.length >= n) {
-		let arr = [];
-		for (let i = n; i > 0; i--) {
-			arr.push(a[a.length - i]);
-		}
-		return arr;
-	}
-	else {
-		return "invalid"
-	}
+function smallerNum(n1, n2) {
+	var x = n1-n2;
+	var y = n2-n1;
+	if (x >= 0){ return n2} else if (y >= 0) {return n1}
 }
