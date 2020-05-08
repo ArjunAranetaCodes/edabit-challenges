e446d9e0d8ffe5c75a6d22b674bdbf2c9c2cd783
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-09 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Fullname-and-Email/JavaScript/solution3.js
+++ Very Easy/Fullname-and-Email/JavaScript/solution3.js
@@ -1,3 +1,8 @@
-function smallerNum(n1, n2) {
-	return +n1 > +n2 ? n2 : n1
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = `${this.firstname} ${this.lastname}`;
+		this.email = `${this.fullname.replace(' ','.').toLowerCase()}@company.com`;
+	}
 }
