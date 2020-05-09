c23d98f73183eb5ff2c61ce157608eeec583fd9a
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-10 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Typing-Game/JavaScript/solution3.js
+++ Very Easy/Typing-Game/JavaScript/solution3.js
@@ -1,8 +1,3 @@
-class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.fullname = `${this.firstname} ${this.lastname}`;
-		this.email = `${this.fullname.replace(' ','.').toLowerCase()}@company.com`;
-	}
+function correctStream(user, correct) {
+	return correct.map((w, i) => user[i] === w ? 1 : -1);
 }
