0171de8fd0720a51996be07bb6b5c53cc857ee0a
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-11 05:00:00 +0800

    Edabit Challenges

--- Very Easy/True-Ones-False-Zeros/JavaScript/solution4.js
+++ Very Easy/True-Ones-False-Zeros/JavaScript/solution4.js
@@ -1,12 +1,1 @@
-function correctStream(user, correct) {
-	var a = [];
-	
-	for(let i = 0; i < user.length; i++) {
-		if(user[i] === correct[i]) {
-			a.push(1)
-		} else {
-			a.push(-1);
-		}
-	}
-	return a
-}
+const integerBoolean = n => [...n].map(x => Boolean(Number(x)));
