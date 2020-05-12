dec47ecf5e88983806f9ea322223f6be4f4e12d9
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-13 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Strange-Pair/JavaScript/solution4.js
+++ Very Easy/Strange-Pair/JavaScript/solution4.js
@@ -1,5 +1,4 @@
-function halloween(dt) {
-  return dt.getMonth() === 9 && dt.getDate() === 31
-    ? "Bonfire toffee"
-    : "toffee";
+function isStrangePair(str1, sstr2) {
+　　return (str1.charAt(0) == sstr2.charAt(sstr2.length-1) &&
+				 sstr2.charAt(0) == str1.charAt(str1.length-1))
 }
