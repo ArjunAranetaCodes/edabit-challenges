4b5138b9ce4876f4c823251a638ae81f4626887e
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-14 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Filter-by-Digit-Length/JavaScript/solution3.js
+++ Very Easy/Filter-by-Digit-Length/JavaScript/solution3.js
@@ -1,5 +1,2 @@
-function isStrangePair(str1, str2) {
-    let a = str1.split('');
-    let b = str2.split('');
-    return (a.pop() == b.shift()) && (a.shift() == b.pop())
-}
+const filterDigitLength = (arr, digits) =>
+  arr.filter(num => String(num).length === digits);
