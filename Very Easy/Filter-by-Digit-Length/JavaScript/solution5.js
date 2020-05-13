89cf111c240a019656badd5b366593f4af3c55e9
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-14 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Filter-by-Digit-Length/JavaScript/solution5.js
+++ Very Easy/Filter-by-Digit-Length/JavaScript/solution5.js
@@ -1,7 +1,9 @@
-function isStrangePair(str1, str2) {
-	if(str1.length < 1 && str2.length < 1) return true
-	if(str1.length < 1 || str2.length < 1) return false
-	if(str1.charAt(0) === str2.charAt(str2.length - 1) && 
-		 	str1.charAt(str1.length - 1) === str2.charAt(0)) return true
-	return false
+function filterDigitLength(arr, num) {
+	var newArr = []
+	for(var x = 0; x < arr.length; x++){
+		if(arr[x].toString().length === num){
+			newArr.push(arr[x])
+		}
+	}
+	return newArr
 }
