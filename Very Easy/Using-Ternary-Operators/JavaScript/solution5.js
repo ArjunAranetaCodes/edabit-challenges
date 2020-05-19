4adc9ad8d6e5c395c626e76dfdb48be2476430e9
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-20 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Using-Ternary-Operators/JavaScript/solution5.js
+++ Very Easy/Using-Ternary-Operators/JavaScript/solution5.js
@@ -1,13 +1,3 @@
-function hammingDistance(str1, str2) {
-
-	 let x = str1.split('');
-	let y = str2.split('');
-	let hamming = 0;
-	for(i = 0; i<x.length;i++){
-		if(x[i]!==y[i]){hamming++}
-	}
-	return hamming;
-	
-	
-	
+function yeah_nope(bool) {
+	return bool == true ? "yeah" : "nope"
 }
