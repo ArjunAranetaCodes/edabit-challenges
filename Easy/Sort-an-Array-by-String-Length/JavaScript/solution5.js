ebadf54e510cafd58fabd0681478e521190c4ae6
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-25 05:00:00 +0800

    Edabit Challenges

--- Easy/Sort-an-Array-by-String-Length/JavaScript/solution5.js
+++ Easy/Sort-an-Array-by-String-Length/JavaScript/solution5.js
@@ -1,7 +1,12 @@
-const isLowerCase = str => str === str.toLowerCase();
-
-const reverseCase = str =>
-  str
-    .split('')
-    .map(char => (isLowerCase(char) ? char.toUpperCase() : char.toLowerCase()))
-    .join('');
+function sortByLength(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length - i - 1; j++) {
+      var temp = array[j]
+      if (array[j].length > array[j + 1].length) {
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
