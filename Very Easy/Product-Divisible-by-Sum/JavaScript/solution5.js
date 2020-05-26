8a328ce9435d92c3289a2c4f5e9af99118bd6f5b
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-27 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Product-Divisible-by-Sum/JavaScript/solution5.js
+++ Very Easy/Product-Divisible-by-Sum/JavaScript/solution5.js
@@ -1,3 +1,9 @@
-function lettersOnly(str) {
-	return str.replace(/[^A-Za-z]/g, "")	
+function divisible(arr) {
+	var prod = 1
+	var sum = 0
+	for(var x = 0; x < arr.length; x++){
+		prod = prod * arr[x]
+		sum = sum + arr[x]
+	}
+	return prod % sum === 0
 }
