5348dc4ba6d358ec970d5fc9e8555eb28ee8a6aa
Arjun Araneta <arjunaraneta@gmail.com>
2020-05-30 05:00:00 +0800

    Edabit Challenges

--- Very Easy/Reverse-and-Capitalize/JavaScript/solution5.js
+++ Very Easy/Reverse-and-Capitalize/JavaScript/solution5.js
@@ -1,1 +1,7 @@
-let formatDate = u => u.split('/').reverse().join('');
+function reverseCapitalize(str) {
+	let reversed = "";
+	for( let i = str.length -1; i >= 0; i--) {
+		reversed += str[i];
+	}
+	return reversed.toUpperCase();
+}
